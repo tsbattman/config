8ca2a9ceeaf81ef2d12af08e0ed8cc2498e14dd6
Thayne Batty <tsbattman@gmail.com>
2013-07-29 15:12:02 -0400

    fix error when glob fails

--- configmapper.sh
+++ configmapper.sh
@@ -3,8 +3,14 @@
 CONFIG_DIR=$HOME
 
 for f in `ls`; do
-  if [ "$f" = "configmapper.sh" ] || [ -e $HOME/.$f ]; then
+  if [ "$f" = "configmapper.sh" ] || [ "$f" = "bin" ] || [ -e $CONFIG_DIR/.$f ]; then
     continue
   fi
   ln -sfT  $PWD/$f $CONFIG_DIR/.$f
+done
+
+if [ ! -d $CONFIG_DIR/bin ]; then mkdir -p $CONFIG_DIR/bin; fi
+
+for b in `ls bin`; do
+  ln -sfT $PWD/bin/$b $CONFIG_DIR/bin/$b
 done
