74cdd0bdf91f4cdab9ad5df3abfcdba7dc311e00
Thayne Batty <tsbattman@gmail.com>
2013-10-28 17:04:28 -0400

    fix vim solarized path. remove xmodmpa

--- configmapper.sh
+++ configmapper.sh
@@ -7,7 +7,7 @@ else OPTS='-sfT'; fi
 mkdir -p $CONFIG_DIR/thirdparty/vim
 if executable git; then
   [[ ! -d $CONFIG_DIR/thirdparty/vim/vim-pathogen ]] && git clone https://github.com/tpope/vim-pathogen.git $CONFIG_DIR/thirdparty/vim/vim-pathogen
-  [[ ! -d $CONFIG_DIR/thirdparty/vim/vim-colors-solarized ]] && git clone https://github.com/altercation/vim-colors-solarized.git $CONFIG_DIR/thirdparty/vim/vim-solarized
+  [[ ! -d $CONFIG_DIR/thirdparty/vim/vim-colors-solarized ]] && git clone https://github.com/altercation/vim-colors-solarized.git $CONFIG_DIR/thirdparty/vim/vim-colors-solarized
   [[ ! -d $CONFIG_DIR/.oh-my-zsh ]] && git clone http://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
 
