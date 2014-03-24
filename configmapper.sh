5ecffe65dba1ae1f8db98d164af689fe6bf7a79b
Thayne Batty <tsbattman@gmail.com>
2014-03-24 18:03:18 -0400

    back to solarized

--- configmapper.sh
+++ configmapper.sh
@@ -15,7 +15,7 @@ mkdir -p "$HOME/thirdparty/vim" "$HOME/thirdparty/style"
 if executable git; then
   gitdl http://github.com/Shougo/neobundle.vim "$HOME/thirdparty/vim/bundle/neobundle.vim"
   gitdl http://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-  link "$HOME/thirdparty/vim/bundle" "dot/vim/bundle"
+  gitdl http://github.com/solarized/xresources.git "$HOME/thirdparty/style/solarized-xresources"
   gitdl http://github.com/chriskempson/base16-xresources.git "$HOME/thirdparty/style/base16-xresources"
   if is_bsd; then
     gitdl http://github.com/chriskempson/base16-iterm2.git "$HOME/thirdparty/style/base16-iterm2"
@@ -31,8 +31,10 @@ XDGCONFIG=${XDG_CONFIG_HOME-$HOME/.config}
 [[ ! -d "$XDGCONFIG" ]] && mkdir -p "$XDGCONFIG"
 for p in $PWD/xdg/*; do link "$p" "$XDGCONFIG/$(basename $p)"; done
 
+link "$HOME/thirdparty/vim/bundle" "$PWD/dot/vim/bundle"
+link "$HOME/thirdparty/style/solarized-xresources/solarized" "$HOME/.Xresources"
+
 link "$HOME/.Xresources" "$HOME/.Xresources-x2go"
 link "$HOME/.xsession" "$HOME/.xsession-x2go"
 link "$HOME/.xsessionrc" "$HOME/.xsessionrc-x2go"
 
-link "$HOME/thirdparty/style/base16-xresources/base16-default.dark.xresources" "$HOME/.Xresources"
