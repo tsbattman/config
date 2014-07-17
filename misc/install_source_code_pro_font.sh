89a5d7378403ff77b9122c98ec6856b1ab0215f9
Thayne Batty <tsbattman@gmail.com>
2014-07-17 11:16:23 -0400

    install sf pro font

--- misc/install_source_code_pro_font.sh
+++ misc/install_source_code_pro_font.sh
@@ -1,4 +1,16 @@
 #! /bin/sh
 
-curl -o '#1' 'http://nchc.dl.sourceforge.net/project/sourcecodepro.adobe/{SourceCodePro_FontsOnly-1.017.zip}'
+fnm=SourceCodePro_FontsOnly-1.017.zip
+pth="$HOME/thirdparty/style/fonts/$fnm"
+
+if [[ ! -e "$pth" ]]; then
+  mkdir -p `dirname $pth`
+  curl -o "$pth" "http://nchc.dl.sourceforge.net/project/sourcecodepro.adobe/$fnm"
+fi
+
+mkdir -p ~/.fonts
+pushd ~/.fonts
+unzip "$pth"
+popd
+fc-cache -v -f
 
