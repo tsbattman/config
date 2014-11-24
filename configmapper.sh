22f2d80a0ca086d1813bc38407d31fed57053cd3
Thayne Batty <tsbattman@gmail.com>
2014-11-24 16:19:55 +0800

    update ghc, cabal location for hvr dpkg. link to haskell executables

--- configmapper.sh
+++ configmapper.sh
@@ -17,18 +17,29 @@ function gitdl() {
 }
 function extern() { echo "$HOME/thirdparty/$1" }
 
-mkdir -p "$(extern vim)" "$(extern style)"
-if executable git; then
-  gitdl http://github.com/Shougo/neobundle.vim "$(extern vim/bundle/neobundle.vim)"
-  gitdl http://github.com/robbyrussell/oh-my-zsh.git "$(extern zsh/oh-my-zsh)"
-  gitdl http://github.com/zsh-users/zsh-completions.git "$(extern zsh/zsh-completions)"
-  #gitdl http://github.com/solarized/xresources.git "$(extern style/solarized-xresources)"
-  if is_bsd; then
-    #gitdl http://github.com/chriskempson/base16-iterm2.git "$HOME/thirdparty/style/base16-iterm2"
-    #curl -G --create-dirs -o "$(extern style/solarized-iterm2/#1)" 'https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/{Solarized%20Dark.itermcolors}'
+UPDATE_EXTERNAL=false
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    '-update-external') UPDATE_EXTERNAL=true ;;
+  esac
+  shift
+done
+
+if $UPDATE_EXTERNAL; then
+  mkdir -p "$(extern vim)" "$(extern style)"
+  if executable git; then
+    gitdl http://github.com/Shougo/neobundle.vim "$(extern vim/bundle/neobundle.vim)"
+    gitdl http://github.com/robbyrussell/oh-my-zsh.git "$(extern zsh/oh-my-zsh)"
+    gitdl http://github.com/zsh-users/zsh-completions.git "$(extern zsh/zsh-completions)"
+    #gitdl http://github.com/solarized/xresources.git "$(extern style/solarized-xresources)"
+    if is_bsd; then
+      #gitdl http://github.com/chriskempson/base16-iterm2.git "$HOME/thirdparty/style/base16-iterm2"
+      #curl -G --create-dirs -o "$(extern style/solarized-iterm2/#1)" 'https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/{Solarized%20Dark.itermcolors}'
+    fi
+    #gitdl http://github.com/chriskempson/base16-xresources.git "$HOME/thirdparty/style/base16-xresources"
+    #gitdl http://github.com/creationix/nvm.git "$(extern js/nvm)"
   fi
-  #gitdl http://github.com/chriskempson/base16-xresources.git "$HOME/thirdparty/style/base16-xresources"
-  #gitdl http://github.com/creationix/nvm.git "$(extern js/nvm)"
 fi
 
 for f in dot/*; do
@@ -42,6 +53,19 @@ done
 
 [[ ! -d "$HOME/bin" ]] && mkdir -p "$HOME/bin"
 for b in bin/*; do link "$PWD/$b" "$HOME/$b"; done
+
+function hask_link () {
+  dir="$(extern haskell/$1/.cabal-sandbox/bin)"; shift
+  [[ -d "$dir" ]] && {
+    for f in "$@"; do
+      link "$dir/$f" "$HOME/bin/$f"
+    done
+  }
+}
+
+hask_link dbmigrations moo
+hask_link ghc-mod ghc-mod ghc-modi hlint
+hask_link hoogle hoogle
 
 XDGCONFIG=${XDG_CONFIG_HOME-$HOME/.config}
 [[ ! -d "$XDGCONFIG" ]] && mkdir -p "$XDGCONFIG"
