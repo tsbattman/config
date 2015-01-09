c060e5eac44504aca2a980713e328d5753f392b4
Thayne Batty <tsbattman@gmail.com>
2015-01-09 18:29:31 +0800

    xmonad brightness. dl xresources

--- configmapper.sh
+++ configmapper.sh
@@ -33,7 +33,7 @@ if $UPDATE_EXTERNAL; then
     gitdl http://github.com/Shougo/neobundle.vim "$(extern vim/bundle/neobundle.vim)"
     gitdl http://github.com/robbyrussell/oh-my-zsh.git "$(extern zsh/oh-my-zsh)"
     gitdl http://github.com/zsh-users/zsh-completions.git "$(extern zsh/zsh-completions)"
-    #gitdl http://github.com/solarized/xresources.git "$(extern style/solarized-xresources)"
+    gitdl http://github.com/solarized/xresources.git "$(extern style/solarized-xresources)"
     if is_bsd; then
       #gitdl http://github.com/chriskempson/base16-iterm2.git "$HOME/thirdparty/style/base16-iterm2"
       #curl -G --create-dirs -o "$(extern style/solarized-iterm2/#1)" 'https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/{Solarized%20Dark.itermcolors}'
--- dot/xmonad/xmonad.hs
+++ dot/xmonad/xmonad.hs
@@ -1,6 +1,20 @@
 
+import qualified Data.Map as Map
+
 import XMonad
+import Graphics.X11.ExtraTypes.XF86
+
+keyMap conf = Map.union (keys conf conf) brightness
+  where
+    brightness = Map.fromList [
+        ((noModMask, xF86XK_MonBrightnessUp), spawn "xbrightness -inc 40")
+      , ((noModMask, xF86XK_MonBrightnessDown), spawn "xbrightness -dec 40")
+      ]
 
 main :: IO ()
-main = xmonad $ defaultConfig { borderWidth = 1, terminal = "urxvt" }
+main = xmonad $ defaultConfig {
+    borderWidth = 1
+  , terminal = "urxvt"
+  , keys = keyMap
+  }
 
