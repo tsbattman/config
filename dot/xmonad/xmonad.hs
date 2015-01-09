b1d4cff62f18b252471fad75d5469996f6b06684
Thayne Batty <tsbattman@gmail.com>
2015-01-09 18:51:24 +0800

    fix brightness. remove julia syntax

--- dot/xmonad/xmonad.hs
+++ dot/xmonad/xmonad.hs
@@ -4,11 +4,11 @@ import qualified Data.Map as Map
 import XMonad
 import Graphics.X11.ExtraTypes.XF86
 
-keyMap conf = Map.union (keys conf conf) brightness
+keyMap conf = keys defaultConfig conf `Map.union`  brightness
   where
     brightness = Map.fromList [
-        ((noModMask, xF86XK_MonBrightnessUp), spawn "xbrightness -inc 40")
-      , ((noModMask, xF86XK_MonBrightnessDown), spawn "xbrightness -dec 40")
+        ((noModMask, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 10")
+      , ((noModMask, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10")
       ]
 
 main :: IO ()
