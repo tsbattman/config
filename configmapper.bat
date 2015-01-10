695f698dccc28f0e006145d7752e44229c92684b
Thayne Batty <tsbattman@gmail.com>
2015-01-10 19:37:03 +0800

    configmapper.bat

---
+++ configmapper.bat
@@ -0,0 +1,5 @@
+
+C:\Windows\System32\MKLINK /D %HOMEPATH%\vimfiles %HOMEPATH%\Documents\dev\config\dot\vim
+C:\Windows\System32\MKLINK %HOMEPATH%\_vimrc %HOMEPATH%\Documents\dev\config\dot\vimrc
+
+C:\Windows\System32\MKLINK %HOMEPATH%\.gitconfig %HOMEPATH%\Documents\dev\config\config\git\config
