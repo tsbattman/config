3d61d88a254f388700f0c9310c4502c23f9ab4f2
Thayne Batty <tsbattman@gmail.com>
2016-05-07 12:33:35 +0800

    config.yaml rather than global-project/stack.yaml

--- configmapper.bat
+++ configmapper.bat
@@ -7,5 +7,5 @@ MKLINK %HOMEPATH%\_vimrc %CONFIGDIR%\dot\vimrc
 MKLINK %HOMEPATH%\.gitconfig %CONFIGDIR%\xdg\git\config
 MKLINK %HOMEPATH%\Documents\.Rprofile %CONFIGDIR%\dot\Rprofile
 
-MKLINK %APPDATA%\stack\stack.yaml %CONFIGDIR%\xdg\stack\stack.yaml
-MKLINK %HOMEPATH%\ghc\ghci.conf %CONFIGDIR%\dot\ghc\ghci.conf
+MKLINK %APPDATA%\stack\config.yaml %CONFIGDIR%\dot\stack\config.yaml
+MKLINK %APPDATA%\ghc\ghci.conf %CONFIGDIR%\dot\ghc\ghci.conf
