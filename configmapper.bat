5f75c2ae36a8b62988bad044877a9c7294f19f7c
Thayne Batty <tsbattman@gmail.com>
2018-11-12 11:26:19 +0800

    more git_ssh comments, enable-putty-support, need to test on linux where putty doesn't exist'

--- configmapper.bat
+++ configmapper.bat
@@ -13,6 +13,7 @@ MKLINK %APPDATA%\ghc\ghci.conf %CONFIGDIR%\dot\ghc\ghci.conf
 REM In widonws, gpg-agent.conf should include "enable-putty-support" and
 REM GIT_SSH env variable is set to plink (program files/putty/plink.exe)
 REM In powershell, $env:GIT_SSH = "C:\Program Files (x86)\putty\plink.exe"
+REM or $env:GIT_SSH = "c:\ProgramData\chocolately\bin\plink.exe"
 MKLINK %APPDATA%\gnupg\gpg-agent.conf %CONFIGDIR%\dot\gnupg\gpg-agent.conf
 
 COPY %CONFIGDIR%\etc\hosts %SystemRoot%\system32\drivers\etc\hosts
