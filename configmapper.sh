10c10a608e6b46864d3d8f60d770f9ea0bfce743
Thayne Batty <tsbattman@gmail.com>
2019-09-05 08:13:46 -0500

    fixes for debian

--- configmapper.sh
+++ configmapper.sh
@@ -117,14 +117,7 @@ function hask_build () {
 }
 if which cabal >> /dev/null ;
 then
-  hask_build hledger-1.14.2   hledger
-  hask_build pandoc-2.7.3     pandoc
-  hask_build hlint-2.1.3      hlint
-  hask_build hpack-0.28.2     hpack
-  # hask_build packunused-0.1.2 packunused
-  hask_build threadscope-0.2.11.1 threadscope
-  hask_build darcs-2.14.2     darcs
-
+  mkdir -p $(extern hs)
   if [[ "$(uname)" != "Darwin" ]]; then
     hask_build xmobar-0.29.5      xmobar "with_xft with_alsa"
     pushd "$PWD/dot/xmonad"
@@ -132,6 +125,14 @@ then
     hask_link $PWD xmonconf-0.1.0.0 xmonconf xmonad-$(uname -m)-$(uname -s | tr 'A-Z' 'a-z')
     popd
   fi
+
+  hask_build hledger-1.14.2   hledger
+  hask_build pandoc-2.7.3     pandoc
+  hask_build hlint-2.1.3      hlint
+  hask_build hpack-0.28.2     hpack
+  # hask_build packunused-0.1.2 packunused
+  hask_build threadscope-0.2.11.1 threadscope
+  hask_build darcs-2.14.2     darcs
 fi
 
 [[ ! -d "${XDG_CONFIG_HOME=$HOME/.config}" ]] && mkdir -p "$XDG_CONFIG_HOME"
