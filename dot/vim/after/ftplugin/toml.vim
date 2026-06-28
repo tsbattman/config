55da9bbc4778e77d6a1f0783f727d7483890bffa
Thayne Batty <tsbattman@icloud.com>
2026-06-27 21:10:50 -0500

    update urls, toml linter

---
+++ dot/vim/after/ftplugin/toml.vim
@@ -0,0 +1,4 @@
+
+let b:ale_fixers=['remove_trailing_lines', 'trim_whitespace']
+let b:ale_linters=['tombi_lint']
+let b:ale_fix_on_save=1
--- xdg/nixpkgs/overlays.nix
+++ xdg/nixpkgs/overlays.nix
@@ -1,5 +1,14 @@
 [
   (self: super: {
-    youtube-dl = super.python3.pkgs.callPackage ./youtube-dl {};
+    youtube-dl = super.python3.pkgs.callPackage ./youtube-dl { };
   })
+  # (self: super: {
+  #   claude-code = super.claude-code.override (oldArgs: {
+  #     version = "2.1.19";
+  #     src = super.fetchzip {
+  #       url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
+  #       hash = "";
+  #     };
+  #   });
+  # })
 ]
